1f31c425a26e2f1e9a9c3461c9687215312fe99d
colopisalvatore <salvatorecolopi1989@gmail.com>
2023-01-18 23:04:02 +0100

    fix migration

--- database/migrations/2023_01_12_093837_create_posts_table.php
+++ database/migrations/2023_01_12_093837_create_posts_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 150)->unique();
+            $table->string('slug');
+            $table->text('content')->nullable();
             $table->timestamps();
         });
     }
--- database/migrations/2023_01_12_095816_add_category_id_to_posts_table.php
+++ database/migrations/2023_01_12_095816_add_category_id_to_posts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->unsignedBigInteger('category_id')->nullable()->after('user_id');
+            $table->unsignedBigInteger('category_id')->nullable()->after('id');
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('set null');
         });
     }
